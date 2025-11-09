0dbfb5923e193a98b9b1283871b181c794f2b4fd
de-mirage2 <mirajparikh3@gmail.com>
2025-11-09 11:57:59 -0600

    zathura fix + swallow regex fix for nvim/vimtex, should work as desired

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,4 +1,6 @@
-vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_view_method = "zathura"
+-- vim.g.vimtex_view_method = "general"
+-- vim.g.vimtex_view_general_viewer = "zathura"
 vim.g.vimtex_compiler_latexmk = {
   options = {
     '-shell-escape',
